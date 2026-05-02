384422b0a340ee018280863e6460e079b39ee82a
kecb-2026 <portner@eids.ch>
2026-05-02 22:02:50 +0200

    app.py aktualisieren

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import time
 st.set_page_config(layout="wide", page_title="KECB Burgdorf 2026", page_icon="🐾")
 
 # Zentrale Logo URL
-
+LOGO_URL = "logo.gif"
 st.image(LOGO_URL)
 st.markdown("""
     <style>
