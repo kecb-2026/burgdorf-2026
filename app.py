8ccd3bd63b113edd3ba8a6c2a16a738eb504abf5
kecb-2026 <portner@eids.ch>
2026-05-04 00:06:52 +0200

    app.py aktualisieren

--- app.py
+++ app.py
@@ -470,7 +470,7 @@ elif st.session_state.view == "BIS_Public":
                             """, unsafe_allow_html=True)
                 else: 
                     st.markdown("<div class='placeholder-box'>🔒</div>", unsafe_allow_html=True)
-                    st_autorefresh(interval=3000, key="bis_refresh")
+    st_autorefresh(interval=3000, key="bis_refresh_global")
     #time.sleep(3)
     #st.rerun()
 
