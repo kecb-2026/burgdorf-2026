57797de3881a472d28509000071a2a6799d21fbc
kecb-2026 <portner@eids.ch>
2026-05-04 00:00:30 +0200

    app.py aktualisieren

--- app.py
+++ app.py
@@ -498,7 +498,7 @@ elif st.session_state.view == "Dashboard":
                                 tags = "".join([f"<span class='tag tag-{t.lower().replace(' ', '')}'>{t}</span> " for t, val in v.items() if val])
                                 st.markdown(f"<div class='cat-card'><div class='cat-number'>{k.split('|')[0]}</div><div class='cat-details'>{get_full_label(m.iloc[0])}</div><div class='tag-container'>{tags}</div></div>", unsafe_allow_html=True)
     #time.sleep(3); st.rerun()
-    st_autorefresh(interval=3000, key="bis_refresh")
+    st_autorefresh(interval=3000, key="dash_refresh")
 
 # STEWARD PANEL
 elif st.session_state.view == "Steward_Panel":
