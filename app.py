1c9aba8ba42436bb51f3e51c44b1a9226c835846
kecb-2026 <portner@eids.ch>
2026-05-03 23:51:52 +0200

    app.py aktualisieren

--- app.py
+++ app.py
@@ -469,9 +469,9 @@ elif st.session_state.view == "BIS_Public":
                             """, unsafe_allow_html=True)
                 else: 
                     st.markdown("<div class='placeholder-box'>🔒</div>", unsafe_allow_html=True)
-
-    time.sleep(3)
-    st.rerun()
+     st_autorefresh(interval=3000, key="bis_refresh")
+    #time.sleep(3)
+    #st.rerun()
 
 
 
@@ -496,7 +496,8 @@ elif st.session_state.view == "Dashboard":
                             if not m.empty:
                                 tags = "".join([f"<span class='tag tag-{t.lower().replace(' ', '')}'>{t}</span> " for t, val in v.items() if val])
                                 st.markdown(f"<div class='cat-card'><div class='cat-number'>{k.split('|')[0]}</div><div class='cat-details'>{get_full_label(m.iloc[0])}</div><div class='tag-container'>{tags}</div></div>", unsafe_allow_html=True)
-    time.sleep(3); st.rerun()
+    #time.sleep(3); st.rerun()
+    st_autorefresh(interval=3000, key="bis_refresh")
 
 # STEWARD PANEL
 elif st.session_state.view == "Steward_Panel":
