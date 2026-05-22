93bddeab1dcd5efac9b34f03a3179c033e16c8bb
kecb-2026 <portner@eids.ch>
2026-05-23 01:00:22 +0200

    app.py aktualisieren

--- app.py
+++ app.py
@@ -388,7 +388,7 @@ def render_overlay_html(row):
             <div class="ov-cat-name">{name_gross}</div>
             <div class="ov-owner">{besitzer}</div>
             <div style="margin-top: 50px;">
-                <img src="{LOGO_URL}" width="100">
+                <img src="https://burgdorf-2026-ykralltanrq8aabhwrarmf.streamlit.app/main/logo_kecb.png" width="200">
                 <div style="font-weight: bold; font-size: 22px; color: #1a4a9e; margin-top: 10px;">KECB BURGDORF 2026</div>
             </div>
         </div>
