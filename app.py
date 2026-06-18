097dd6aeebf342f208a5a58f92b7d5617ec5f116
kecb-2026 <portner@eids.ch>
2026-06-18 23:54:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2997,6 +2997,16 @@ elif st.session_state.view == "Test_Live_Admin":
 
     display_header_with_logo("👑 BIS Admin Control")
     df_full = load_labels()
+
+	# Debug-Check nach dem Laden
+store = GlobalStore()
+store.load_backup()
+
+st.sidebar.write("---")
+st.sidebar.write("DEBUG: Geladene Votes keys:")
+st.sidebar.write(list(store.data.get("votes", {}).keys()))
+
+
     
     if df_full is not None:
         # --- HIER DIE NEUE RADIO-BOX FÜR DIE TAGE EINFÜGEN ---
