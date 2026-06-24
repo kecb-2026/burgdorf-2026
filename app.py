3256cc16b010d76d64e9e7f21fb5a3c12892145e
kecb-2026 <portner@eids.ch>
2026-06-24 17:27:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ from reportlab.lib import colors
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
-<style>
+    <style>
     #root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 0rem;}
-</style>
+    </style>
 
 # --- 1. SETUP & STYLING ---
 st.set_page_config(layout="wide", page_title="KECB Burgdorf 2026", page_icon="🐾")
