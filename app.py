37d9989b7c74d9c46459bf0f4da56c64d6d2e899
kecb-2026 <portner@eids.ch>
2026-08-23 00:46:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1267,10 +1267,14 @@ elif st.session_state.view == "Steward_Panel":
                 farbgruppe = row[fg_cols[0]] if fg_cols else row.get('FARBGRUPPE', 'N/A')
                 if pd.isna(farbgruppe) or str(farbgruppe).strip().lower() == "nan": farbgruppe = "N/A"
                 
-                geschlecht = row.get('GESCHLECHT', 'N/A')
-                
-                kastriert_val = str(row.get('Kastriert', '')).strip().upper()
-                if kastriert_val == 'X':
+                #geschlecht = row.get('GESCHLECHT', 'N/A')
+                #kastriert_val = str(row.get('Kastriert', '')).strip().upper()
+                #if kastriert_val == 'X':
+                    #geschlecht = f"{geschlecht} (Kastriert)"
+
+				geschlecht = row.get('GESCHLECHT', 'N/A')
+                kastriert_val = str(row.get('Kastriert', '')).strip()
+                if kastriert_val != '':
                     geschlecht = f"{geschlecht} (Kastriert)"
 
 				
