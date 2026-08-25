aed6bec0f79bbbee49bd354b4262eee58863de00
kecb-2026 <portner@eids.ch>
2026-08-25 03:20:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -270,9 +270,9 @@ st.markdown("""
         height: 90px; width: 100%; line-height: 1.1; 
     }
 
-    .cat-card, .placeholder-box { height: 95px; padding: 5px; border: 2px solid #1a4a9e; text-align: center; background-color: #f0f0f0; border-radius: 14px; margin-bottom: 5px; min-height: 90px; display: flex; flex-direction: column; justify-content: center; align-items: center; }
-    .placeholder-box { height: 95px; border: 1px solid #d1d1d1; background-color: #f2f2f2 !important; color: #999999; }
-    .winner-card { height: 99px; border: 1px solid #ff4d4d !important; background-color: #ffcccc !important; color: #b21f2d !important; }
+    .cat-card, .placeholder-box { height: 90px; padding: 5px; border: 2px solid #1a4a9e; text-align: center; background-color: #f0f0f0; border-radius: 14px; margin-bottom: 5px; min-height: 90px; display: flex; flex-direction: column; justify-content: center; align-items: center; }
+    .placeholder-box { height: 90px; border: 1px solid #d1d1d1; background-color: #f2f2f2 !important; color: #999999; }
+    .winner-card { height: 90px; border: 1px solid #ff4d4d !important; background-color: #ffcccc !important; color: #b21f2d !important; }
     .cat-number { font-size: 28px !important; font-weight: 900 !important; color: #1a4a9e; line-height: 1.0; }
     .cat-details { font-size: 14px !important; color: #333; font-weight: bold; margin-top: 2px; line-height: 1.1; }
 
